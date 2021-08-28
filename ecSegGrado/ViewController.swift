03c9f3fb65571d783caf4ec9090aa5667c52a0ed
ErnestoLV <ernestolopezvv@outlook.com>
2021-08-27 22:35:58 -0600

    Initial Commit

---
+++ ecSegGrado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ecSegGrado
+//
+//  Created by Ernesto López on 27/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
